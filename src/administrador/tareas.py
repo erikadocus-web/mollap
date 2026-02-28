07f1113f25d2abbfc93ce79aacc1b2123a4c88e1
erikadocus <erikadocus@gmail.com>
2026-02-28 20:19:45 +0100

    fix(Tareas): Refactorizar y añadir condicional

--- src/administrador/tareas.py
+++ src/administrador/tareas.py
@@ -1,21 +1,23 @@
 class Tareas():
     def __init__(self):
-        self.tareas = []
+        self.listaTareas = []
         self.objetivo = None
 
     def getTareas(self):
-        return self.tareas
+        return self.listaTareas
     
     def getObjetivo(self):
         return self.objetivo
     
-    def getTarea(self,tarea):
-        self.tareas.append(tarea)
+    def añadirTarea(self, tarea):
+        self.listaTareas.append(tarea)
     
     def setObjetivo(self, objetivo):
         self.objetivo = objetivo
 
-    def ejecutarTareas(self, objetivo):
-        for tarea in self.tareas:
-            tarea.ejecutar(objetivo)
-   
+    def ejecutarTareas(self, mensaje):
+        for tarea in self.listaTareas:
+            tarea.ejecutar(mensaje)
+        
+        if self.objetivo:
+            self.objetivo.ejecutar(mensaje)
